03b5a879826f46498f30e799764657cc6acee80f
Lawrence Brakmo <brakmo@fb.com>
2018-01-25 16:14:14 -0800

    bpf: Add BPF_SOCK_OPS_RETRANS_CB
    Adds support for calling sock_ops BPF program when there is a
    retransmission. Three arguments are used; one for the sequence number,
    another for the number of segments retransmitted, and the last one for
    the return value of tcp_transmit_skb (0 => success).
    Does not include syn-ack retransmissions.
    
    New op: BPF_SOCK_OPS_RETRANS_CB.
    
    Signed-off-by: Lawrence Brakmo <brakmo@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2776,6 +2776,15 @@ int __tcp_retransmit_skb(struct sock *sk, struct sk_buff *skb, int segs)
 		err = tcp_transmit_skb(sk, skb, 1, GFP_ATOMIC);
 	}
 
+	//#ifdef VENDOR_EDIT
+	//Add for TCP Retransmit info send to user space.
+	oppo_handle_retransmit(sk, -1); // in this function, tcp_transmit_skb is called again.
+	//#endif /* VENDOR_EDIT */
+
+	if (BPF_SOCK_OPS_TEST_FLAG(tp, BPF_SOCK_OPS_RETRANS_CB_FLAG))
+		tcp_call_bpf_3arg(sk, BPF_SOCK_OPS_RETRANS_CB,
+				  TCP_SKB_CB(skb)->seq, segs, err);
+
 	if (likely(!err)) {
 		segs = tcp_skb_pcount(skb);
 
